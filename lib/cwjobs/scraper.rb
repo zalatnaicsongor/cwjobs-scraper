6f836d74a8e6c36c435ac8ec14216118e91233ae
Csongor Zalatnai <csongor@zalatnai.hu>
2015-10-25 21:28:19 +0100

    initialized gem structure

---
+++ lib/cwjobs/scraper.rb
@@ -0,0 +1,7 @@
+require "cwjobs/scraper/version"
+
+module Cwjobs
+  module Scraper
+    # Your code goes here...
+  end
+end
---
+++ lib/cwjobs/scraper/version.rb
@@ -0,0 +1,5 @@
+module Cwjobs
+  module Scraper
+    VERSION = "0.1.0"
+  end
+end
